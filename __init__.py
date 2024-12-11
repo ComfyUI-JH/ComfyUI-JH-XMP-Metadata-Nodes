8b5b812e1422320c76dbafcdcce9ef1e6dcd9c2b
Jeffery Harrell <jefferyharrell@gmail.com>
2024-12-11 06:46:04 -0800

    Added Path to Stem node

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 from .jh_save_image_with_xmp_metadata import JHSaveImageWithXMPMetadata
 from .jh_save_png_with_xmp_metadata import JHSavePNGWithXMPMetadata
 from .jh_save_webp_with_xmp_metadata import JHSaveWebPWithXMPMetadata
+from .jh_path_to_stem import JHPathToStem
 
 NODE_CLASS_MAPPINGS = {
     "JHSaveImageWithXMPMetadata": JHSaveImageWithXMPMetadata,
     "JHSavePNGWithXMPMetadata": JHSavePNGWithXMPMetadata,
     "JHSaveWebPWithXMPMetadata": JHSaveWebPWithXMPMetadata,
+    "JHPathToStem": JHPathToStem,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "JHSaveImageWithXMPMetadata": "Save Image With XMP Metadata",
     "JHSavePNGWithXMPMetadata": "Save PNG With XMP Metadata",
     "JHSaveWebPWithXMPMetadata": "Save WebP With XMP Metadata",
+    "JHPathToStem": "Path to Stem",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
---
+++ jh_path_to_stem.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+class JHPathToStem:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "path": ("STRING",),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "path_to_stem"
+    CATEGORY = "JHXMP"
+    OUTPUT_NODE = False
+
+    def path_to_stem(self, path):
+        return (Path(path).stem,)
