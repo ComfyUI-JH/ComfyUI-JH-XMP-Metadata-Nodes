40ee901b06d7a88b5240561485a8d3f2023c34fa
Jeffery Harrell <jefferyharrell@gmail.com>
2024-12-11 18:07:11 -0800

    Fixed some formatting oversights

--- tests/test_jh_xmp_metadata.py
+++ tests/test_jh_xmp_metadata.py
@@ -1,5 +1,5 @@
-import pytest
 from src.jh_xmp_metadata import JHXMPMetadata
+
 
 def test_init():
     metadata = JHXMPMetadata()
@@ -11,40 +11,48 @@ def test_init():
     assert metadata.make is None
     assert metadata.model is None
 
+
 def test_set_creator():
     metadata = JHXMPMetadata()
     metadata.creator = "John Doe"
     assert metadata._creator == {"John Doe"}
+
 
 def test_set_title():
     metadata = JHXMPMetadata()
     metadata.title = "My Title"
     assert metadata._title == "My Title"
 
+
 def test_set_description():
     metadata = JHXMPMetadata()
     metadata._description = "My Description"
     assert metadata._description == "My Description"
+
 
 def test_set_subject():
     metadata = JHXMPMetadata()
     metadata.subject = "My Subject"
     assert metadata._subject == {"My Subject"}
 
+
 def test_set_instructions():
     metadata = JHXMPMetadata()
     metadata.instructions = "My Instructions"
     assert metadata._instructions == "My Instructions"
+
 
 def test_set_make():
     metadata = JHXMPMetadata()
     metadata.make = "My Make"
     assert metadata._make == "My Make"
 
+
 def test_set_model():
     metadata = JHXMPMetadata()
     metadata.model = "My Model"
     assert metadata._model == "My Model"
+
 
 def test_to_string():
     metadata = JHXMPMetadata()
@@ -64,6 +72,7 @@ def test_to_string():
     assert "My Instructions" in xml_string
     assert "My Make" in xml_string
     assert "My Model" in xml_string
+
 
 def test_to_wrapped_string():
     metadata = JHXMPMetadata()
