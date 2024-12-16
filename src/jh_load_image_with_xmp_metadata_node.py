378e6bff711ade11016d89455687e6fd057417d1
Jeffery Harrell <jefferyharrell@gmail.com>
2024-12-16 07:43:14 -0800

    Readability improvements

--- src/jh_load_image_with_xmp_metadata_node.py
+++ src/jh_load_image_with_xmp_metadata_node.py
@@ -53,6 +53,8 @@ class JHLoadImageWithXMPMetadataNode:
     OUTPUT_NODE = False
 
     def load_image(self, image):
+        # `image` here is a string, the name of the image file on disk;
+        # just the filename, not the full path.
         image_path = folder_paths.get_annotated_filepath(image)
 
         creator = None
@@ -66,15 +68,16 @@ class JHLoadImageWithXMPMetadataNode:
 
         output_images = []
         output_masks = []
-        w, h = None, None
+        width: int = 0
+        height: int = 0
 
         excluded_formats = ["MPO"]
 
-        for i in PIL.ImageSequence.Iterator(image_object):
+        for raw_frame in PIL.ImageSequence.Iterator(image_object):
 
             # Extract XMP metadata from the first frame, if available
             if len(output_images) == 0:
-                xml_string = i.info.get("xmp", None)
+                xml_string = raw_frame.info.get("xmp", None)
                 if isinstance(xml_string, bytes):
                     xml_string = xml_string.decode("utf-8")
                 if xml_string:  # Ensure xml_string is not None or empty
@@ -85,35 +88,39 @@ class JHLoadImageWithXMPMetadataNode:
                     subject = xmp_metadata.subject
                     instructions = xmp_metadata.instructions
 
-            # Fix image orientation based on EXIF metadata
-            i = PIL.ImageOps.exif_transpose(i)
+            # Fix image orientation based on EXIF metadata. Do this in
+            # place to avoid creating a new image object for each frame.
+            PIL.ImageOps.exif_transpose(raw_frame, in_place=True)
 
             # Convert 32-bit integer images to RGB
-            if i.mode == "I":
-                i = i.point(lambda i: i * (1 / 255))
-            image = i.convert("RGB")
+            if raw_frame.mode == "I":
+                raw_frame = raw_frame.point(lambda i: i * (1 / 255))
+            rgb_frame = raw_frame.convert("RGB")
 
             # Ensure all frames are the same size as the first frame
             if len(output_images) == 0:
-                w = image.size[0]
-                h = image.size[1]
-            if image.size[0] != w or image.size[1] != h:
+                width = rgb_frame.size[0]
+                height = rgb_frame.size[1]
+            if rgb_frame.size[0] != width or rgb_frame.size[1] != height:
                 continue
 
             # Normalize the image to a tensor with values in [0, 1]
-            image = np.array(image).astype(np.float32) / 255.0
-            image = torch.from_numpy(image)[None,]
+            np_array = np.array(rgb_frame).astype(np.float32) / 255.0
+            image_tensor = torch.from_numpy(np_array)[None,]
 
-            # Generate the mask if the frame has an alpha channel, otherwise use an empty mask
-            if "A" in i.getbands():
-                mask = np.array(i.getchannel("A")).astype(np.float32) / 255.0
-                mask = 1.0 - torch.from_numpy(mask)
+            # Go back to the original multichannel(?) frame and extract
+            # the alpha channel as a mask
+            if "A" in raw_frame.getbands():
+                np_array = (
+                    np.array(raw_frame.getchannel("A")).astype(np.float32) / 255.0
+                )
+                mask_tensor = 1.0 - torch.from_numpy(np_array)
             else:
-                mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+                mask_tensor = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
 
             # Append the processed image and mask to the outputs
-            output_images.append(image)
-            output_masks.append(mask.unsqueeze(0))
+            output_images.append(image_tensor)
+            output_masks.append(mask_tensor.unsqueeze(0))
 
         # Combine frames into a single tensor if multiple frames exist
         if len(output_images) > 1 and image_object.format not in excluded_formats:
