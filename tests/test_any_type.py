6af8154702741c0dca71658e97620f18ff0bac47
Jeffery Harrell <jefferyharrell@gmail.com>
2024-12-21 09:26:33 -0800

    Remove tests for AnyType class as it has been refactored into JHAnyType

--- tests/test_any_type.py
+++
@@ -1,7 +0,0 @@
-from comfyui_jh_xmp_metadata_nodes.any_type import AnyType
-
-
-def test_ne_method():
-    any_type_instance = AnyType("test")
-    assert not (any_type_instance != "test")
-    assert not (any_type_instance != "different")
